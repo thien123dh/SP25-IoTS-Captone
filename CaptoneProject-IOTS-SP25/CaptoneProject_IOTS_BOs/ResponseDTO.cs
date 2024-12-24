86ed2dfaaad09b614ad4c1a08508d2950906e5b2
VanHa1006 <106126039+VanHa1006@users.noreply.github.com>
2024-12-24 23:03:26 +0700

    Commit Start Project
    Done Environment BE

---
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_BOs/ResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptoneProject_IOTS_BOs
+{
+    public class ResponseDTO
+    {
+        public bool IsSuccess { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public string Message { get; set; }
+        public object? Data { get; set; }
+    }
+}
