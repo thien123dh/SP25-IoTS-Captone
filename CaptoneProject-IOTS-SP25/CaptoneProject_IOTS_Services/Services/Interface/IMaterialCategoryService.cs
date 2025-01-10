a6161b2483058d7f4f53feef4d5928df45d4c2e0
VanHa1006 <106126039+VanHa1006@users.noreply.github.com>
2025-01-10 12:01:40 +0700

    Commit all

---
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_Services/Services/Interface/IMaterialCategoryService.cs
@@ -0,0 +1,23 @@
+﻿using CaptoneProject_IOTS_BOs.DTO.MaterialCategotyDTO;
+using CaptoneProject_IOTS_BOs.DTO.PaginationDTO;
+using CaptoneProject_IOTS_BOs.Models;
+using CaptoneProject_IOTS_Service.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptoneProject_IOTS_Service.Services.Interface
+{
+    public interface IMaterialCategoryService
+    {
+        Task<IBusinessResult> GetAllMaterialCategory(PaginationRequest paginationRequest);
+        Task<IBusinessResult> GetByIdAsync(string id);
+        Task<IBusinessResult> UpdateAsync(MaterialCategory categoryMaterial);
+        Task<IBusinessResult> CreateMaterialCategory(MatertialCategoryRequestDTO categoryMaterial);
+        Task<IBusinessResult> DeleteAsync(int id);
+        Task<IBusinessResult> GetAllCategories();
+        Task<IBusinessResult> GetById(int id);
+    }
+}
