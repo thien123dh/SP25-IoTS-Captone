4546bc94d4b5fef5eb355d27495a5015badb2dd9
thien123dh <134196273+thien123dh@users.noreply.github.com>
2025-02-06 19:20:02 +0700

    fixing user register bug

--- CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_BOs/Models/User.cs
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_BOs/Models/User.cs
@@ -16,7 +16,7 @@ public partial class User
 
     public string Fullname { get; set; }
 
-    public string? Password { get; set; }
+    public string? Password { get; set; } = "";
 
     public string? Phone { get; set; }
 
