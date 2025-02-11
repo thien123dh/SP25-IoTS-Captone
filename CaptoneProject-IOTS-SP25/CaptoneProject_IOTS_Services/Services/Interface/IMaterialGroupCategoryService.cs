ee3893dfbbccddca730e300559ca4bbe9c121048
VanHa1006 <106126039+VanHa1006@users.noreply.github.com>
2025-02-12 03:17:51 +0700

    Commit Category Group Product

---
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_Services/Services/Interface/IMaterialGroupCategoryService.cs
@@ -0,0 +1,22 @@
+﻿using CaptoneProject_IOTS_BOs.DTO.MaterialCategotyDTO;
+using CaptoneProject_IOTS_BOs.DTO.PaginationDTO;
+using CaptoneProject_IOTS_BOs.Models;
+using CaptoneProject_IOTS_BOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CaptoneProject_IOTS_BOs.DTO.MaterialGroupCategoryDTO;
+
+namespace CaptoneProject_IOTS_Service.Services.Interface
+{
+    public interface IMaterialGroupCategoryService
+    {
+        Task<ResponseDTO> CreateOrUpdateMaterialGroupCategory(int? id, CreateUpdateMaterialGroupCategoryDTO categoryGroupMaterial);
+        Task<ResponseDTO> UpdateMaterialGroupCategoryStatus(int id, int IsActive);
+        Task<ResponseDTO> GetAllMaterialGroupCategory(string searchKeyword);
+        Task<GenericResponseDTO<PaginationResponseDTO<MaterialGroupCategory>>> GetPaginationMaterialGroupCategories(PaginationRequest paginate, int? statusFilter);
+        Task<GenericResponseDTO<MaterialGroupCategory>> GetByMaterialGroupCategoryId(int id);
+    }
+}
