0783e7b85a5cbe86d90eff027da1c2e1562fd5f4
thien123dh <134196273+thien123dh@users.noreply.github.com>
2025-02-23 02:39:07 +0700

    create cart, combo and order

---
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_BOs/Models/Lab.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptoneProject_IOTS_BOs.Models
+{
+    public partial class Lab
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { set; get; }
+    }
+}
