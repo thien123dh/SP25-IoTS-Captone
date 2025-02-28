ab615365777270d089beac403c5db9fd20240561
VanHa1006 <106126039+VanHa1006@users.noreply.github.com>
2025-03-01 03:46:44 +0700

    Commit Order

--- CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_BOs/DTO/OrderDTO/OrderResponeDTO.cs
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_BOs/DTO/OrderDTO/OrderResponeDTO.cs
@@ -13,43 +13,6 @@ namespace CaptoneProject_IOTS_BOs.DTO.OrderDTO
 {
     public class OrderResponeDTO
     {
-        [MaxLength(200)]
-        public string ApplicationSerialNumber { set; get; }
-
-        [ForeignKey(nameof(User))]
-        public int SellerId { set; get; }
-
-        [ForeignKey(nameof(User))]
-        public int OrderBy { set; get; }
-
-        [Precision(18, 1)]
-        public decimal TotalPrice { set; get; } = 0;
-
-        [MaxLength(500)]
-        public string Address { set; get; } = "";
-
-        [MaxLength(150)]
-        public string ContactNumber { set; get; } = "";
-
-        [MaxLength(300)]
-        public string? Notes { set; get; } = "";
-
-        [MaxLength(300)]
-        public string? Remark { set; get; } = "";
-
-        public DateTime CreateDate { set; get; } = DateTime.Now;
-
-        [ForeignKey(nameof(User))]
-        public int? CreatedBy { set; get; }
-
-        public DateTime UpdatedDate { set; get; } = DateTime.Now;
-
-        [ForeignKey(nameof(User))]
-        public int? UpdatedBy { set; get; }
-
-        [Range(1, 15)]
-        public int OrderStatusId { set; get; }
-
-        public List<OrderItemResponeDTO> Details { get; set; }
+        public string PaymentUrl { get; set; }
     }
 }
--- CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_Services/Services/Interface/IOrderService.cs
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_Services/Services/Interface/IOrderService.cs
@@ -6,14 +6,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CaptoneProject_IOTS_BOs.DTO.OrderDTO;
+using CaptoneProject_IOTS_BOs.DTO.VNPayDTO;
 
 namespace CaptoneProject_IOTS_Service.Services.Interface
 {
     public interface IOrderService
     {
         public Task<GenericResponseDTO<OrderResponeDTO>> getOrderDetailsByOrderId(int id);
-        public Task<GenericResponseDTO<OrderResponeDTO>> CreateOrder(int? id, OrderRequestDTO payload);
-        public Task<GenericResponseDTO<OrderResponeDTO>> CheckOrderSuccessfull(int? id, OrderRequestDTO payload);
+        public Task<GenericResponseDTO<OrderResponeDTO>> CreateOrder(int? id, OrderRequestDTO payload, string returnUrl);
+        public Task<GenericResponseDTO<OrderResponeDTO>> CheckOrderSuccessfull(int? id, VNPayRequestDTO dto);
         public Task<GenericResponseDTO<OrderResponeDTO>> GetOrdersByUserID(int userId);
     }
 }
