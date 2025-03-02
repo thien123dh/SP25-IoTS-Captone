2c57c6372bc01ad0fa9b7d655b8852bc4aeefe9a
thien123dh <134196273+thien123dh@users.noreply.github.com>
2025-03-03 01:01:00 +0700

    update combo quantity

---
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_BOs/Validation/NumberAttributeValidation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptoneProject_IOTS_BOs.Validation
+{
+    public class PositiveIntAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is int dateValue)
+            {
+                if ((int)value >= 0)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+            return new ValidationResult("Invalid number format");
+        }
+    }
+
+    public class PositiveDecimalAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is decimal dateValue)
+            {
+                if ((decimal)value >= 0)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+            return new ValidationResult("Invalid number format");
+        }
+    }
+}
