8fbd2392e944feb4bf88a3d306a0a688801b06c0
thien123dh <134196273+thien123dh@users.noreply.github.com>
2025-03-30 02:35:33 +0700

    fixing common search

---
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_BOs/DTO/CommonDTO/CommonDTO.cs
@@ -0,0 +1,24 @@
+﻿using CaptoneProject_IOTS_BOs.DTO.MaterialDTO;
+using CaptoneProject_IOTS_BOs.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptoneProject_IOTS_BOs.DTO.CommonDTO
+{
+    public class CommonSearchRequestDTO
+    {
+        [Required]
+        public string Keyword { set; get; } = "";
+    }
+
+    public class CommonSearchResponseDTO
+    {
+        public List<IotsDevice>? IotDevices { set; get; }
+
+        public List<Combo>? Combos { set; get; }
+    }
+}
---
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_Services/Services/Implement/CommonService.cs
@@ -0,0 +1,41 @@
+﻿using CaptoneProject_IOTS_BOs;
+using CaptoneProject_IOTS_BOs.DTO.CommonDTO;
+using CaptoneProject_IOTS_Service.ResponseService;
+using CaptoneProject_IOTS_Service.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptoneProject_IOTS_Service.Services.Implement
+{
+    public class CommonService : ICommonService
+    {
+        private readonly UnitOfWork unitOfWork;
+        private readonly int DEFAULT_SELECT_TOP = 20;
+        public CommonService(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<GenericResponseDTO<CommonSearchResponseDTO>> RelativeSearchApplication(CommonSearchRequestDTO request)
+        {
+            var devices = unitOfWork.IotsDeviceRepository.Search(
+                item => request.Keyword.ToLower().Contains(item.Name) && item.IsActive > 0
+            ).OrderByDescending(item => item.CreatedDate).Take(DEFAULT_SELECT_TOP).ToList();
+
+            var combos = unitOfWork.ComboRepository.Search(
+                item => request.Keyword.ToLower().Contains(item.Name) && item.IsActive > 0
+            ).OrderByDescending(item => item.CreatedDate).Take(DEFAULT_SELECT_TOP).ToList();
+
+            var res = new CommonSearchResponseDTO
+            {
+                IotDevices = devices,
+                Combos = combos
+            };
+
+            return ResponseService<CommonSearchResponseDTO>.OK(res);
+        }
+    }
+}
