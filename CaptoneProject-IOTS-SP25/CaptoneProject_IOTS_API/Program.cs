bc26c58555eb11e123c8f5d95769696fea4865d9
VanHa1006 <106126039+VanHa1006@users.noreply.github.com>
2025-04-02 03:27:26 +0700

    Commit Again Release Code Chat

--- CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_API/Program.cs
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_API/Program.cs
@@ -202,23 +202,6 @@ builder.Services.AddAuthentication(options =>
         ValidAudience = audience,
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
     };
-
-    options.Events = new JwtBearerEvents
-    {
-        OnMessageReceived = context =>
-        {
-            var accessToken = context.Request.Query["access_token"];
-
-
-            var path = context.HttpContext.Request.Path;
-            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/chathub"))
-            {
-                context.Token = accessToken;
-            }
-
-            return Task.CompletedTask;
-        }
-    };
 });
 
 
