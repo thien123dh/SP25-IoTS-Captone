7cc1803a9ed9405c12674b3062ebea065b04ee60
VanHa1006 <106126039+VanHa1006@users.noreply.github.com>
2025-04-23 10:56:29 +0700

    Commit Fix bug Combo

--- CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_Repository/Repository/Implement/ComboRepository.cs
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_Repository/Repository/Implement/ComboRepository.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,6 +29,11 @@ namespace CaptoneProject_IOTS_Repository.Repository.Implement
             return await _dbSet
                 .Where(combo => combo.CreatedBy == creatorId)
                 .ToListAsync();
+        }
+
+        public bool Any(Expression<Func<Combo, bool>> predicate)
+        {
+            return _dbSet.Any(predicate);
         }
     }
 }
