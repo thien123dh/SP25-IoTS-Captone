fdbf8ddef0f009f80dedb39a0b0ab62a85016176
thien123dh <134196273+thien123dh@users.noreply.github.com>
2025-05-07 23:28:27 +0700

    fixing dto response

--- CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_BOs/DTO/RefundDTO/RefundRequestDTO.cs
+++ CaptoneProject-IOTS-SP25/CaptoneProject_IOTS_BOs/DTO/RefundDTO/RefundRequestDTO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CaptoneProject_IOTS_BOs.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +18,30 @@ namespace CaptoneProject_IOTS_BOs.DTO.RefundDTO
         public string AccountNumber { set; get; }
 
         public string BankName { set; get; }
+    }
+
+    public class RefundRequestDTO
+    {
+        public int Id { set; get; }
+        public decimal Amount { set; get; }
+        public int OrderId { set; get; }
+        public string OrderCode { set; get; }
+        public string ContactNumber { set; get; }
+
+        public string AccountName { set; get; }
+
+        public string AccountNumber { set; get; }
+
+        public string BankName { set; get; }
+        public int CreatedBy { set; get; }
+        public User CreatedByNavigation { set; get; }
+        public DateTime CreatedDate { set; get; }
+        public int ActionBy { set; get; }
+
+        public User ActionByNavigation { set; get; }
+
+        public DateTime ActionDate { set; get; }
+
+        public short Status { set; get; }
     }
 }
